d507cd888b668937dc3e127447347fb9630cfaec
Melina Lanvermann <melina.lanvermann@adesso.de>
2023-12-22 15:34:07 +0100

    Day1

--- Day1.java
+++ Day1.java
@@ -1,2 +1,26 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Day1 {
+    public static void main(String[] args) throws FileNotFoundException {
+        File inputFile = new File("input_day1.txt");
+        Scanner readFile = new Scanner(inputFile);
+        int sumOfAll = 0;
+        ArrayList<String> linesOfFile = new ArrayList<String>();
+
+        while (readFile.hasNextLine()) {
+            linesOfFile.add(readFile.nextLine().replaceAll("\\D+", ""));
+        }
+        readFile.close();
+        for (int i = 0; i < linesOfFile.size(); i++) {
+            String thisLine = linesOfFile.get(i);
+            int first = Integer.parseInt(String.valueOf(thisLine.charAt(0)))*10;
+            int last = Integer.parseInt(String.valueOf(thisLine.charAt(thisLine.length()-1)));
+            sumOfAll += (first+last);
+        }
+        System.out.println(sumOfAll);
+    }
 }
