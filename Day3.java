49c1fd8534f77b7f56a1640c2f2b3891f441a0ac
Melina Lanvermann <melina.lanvermann@adesso.de>
2024-01-09 11:46:21 +0100

    pls halp

--- Day3.java
+++ Day3.java
@@ -1,7 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.sql.SQLOutput;
+import java.util.*;
 
 public class Day3 {
     public static void main(String[] args) throws FileNotFoundException {
@@ -19,7 +19,6 @@ public class Day3 {
         char[][] charArray = new char[rows][columns];
         ArrayList<String> schematicNumbers = new ArrayList<String>();
         int engineSum = 0;
-
         // copy to 2D Array
         for (int i = 0; i < rows; i++) {
             String thisLine = fileLines.get(i);
@@ -27,113 +26,79 @@ public class Day3 {
                 charArray[i][u] = thisLine.charAt(u);
             }
         }
-        int j = 0;
-        while (j < rows) {
-            int k = 0;
-            String foundNumber = "";
-            while (k < columns) {
-                boolean markerFound = false;
-                if (isNumber(charArray[j][k])) {
-                    foundNumber = foundNumber.concat(String.valueOf(charArray[j][k]));
-                    for (int c = 0; c < 8; c++) {
-                        int schematicRow = j;
-                        int schematicCol = k;
-                        System.out.println("starting to check...");
-                        while (c == 0) {
-                            schematicRow--;
-                            if (schematicRow < 0) {
-                                schematicRow = 0;
-                            }
-                            System.out.println("checking : " + schematicRow + "/" + schematicCol);
-                            if (isSchematicMarker(charArray[schematicRow][schematicCol])) {
-                                markerFound = true;
-                                System.out.println("I found a marker at : " + schematicRow + "/" + schematicCol);
-                                c++;
-                            } else {
-                                c++;
-                            }
-                        }
-                        while (c == 1) {
-                            schematicCol--;
-                            if (schematicCol < 0) {
-                                schematicCol = 0;
-                            }
-                            System.out.println("checking : " + schematicRow + "/" + schematicCol);
-                            if (isSchematicMarker(charArray[schematicRow][schematicCol])) {
-                                markerFound = true;
-                                System.out.println("I found a marker at : " + schematicRow + "/" + schematicCol);
-                                c++;
-                            } else {
-                                c++;
-                            }
-                        }
-                        while (c > 1 && c < 4) {
-                            schematicRow++;
-                            if (schematicRow > rows - 1) {
-                                schematicRow = rows - 1;
-                            }
-                            System.out.println("checking : " + schematicRow + "/" + schematicCol);
-                            if (isSchematicMarker(charArray[schematicRow][schematicCol])) {
-                                markerFound = true;
-                                System.out.println("I found a marker at : " + schematicRow + "/" + schematicCol);
-                                c++;
-                            } else {
-                                c++;
-                            }
-                        }
-                        while (c > 3 && c < 6) {
-                            schematicCol++;
-                            if (schematicCol > columns - 1) {
-                                schematicCol = columns - 1;
-                            }
-                            System.out.println("checking : " + schematicRow + "/" + schematicCol);
-                            if (isSchematicMarker(charArray[schematicRow][schematicCol])) {
-                                markerFound = true;
-                                System.out.println("I found a marker at : " + schematicRow + "/" + schematicCol);
-                                c++;
-                            } else {
-                                c++;
-                            }
-                        }
-                        while (c > 5 && c < 8) {
-                            schematicRow--;
-                            if (schematicRow < 0) {
-                                schematicRow = 0;
-                            }
-                            System.out.println("checking : " + schematicRow + "/" + schematicCol);
-                            if (isSchematicMarker(charArray[schematicRow][schematicCol])) {
-                                markerFound = true;
-                                System.out.println("I found a marker at : " + schematicRow + "/" + schematicCol);
-                                c++;
-                            } else {
-                                c++;
-                            }
-                        }
 
-                    }
-                    try {
-                        if (!isNumber(charArray[j][k + 1])) {
-                            if (markerFound) {
-                                System.out.println("I have found schematic number : " + foundNumber + ".\n The current engine sum is : " + engineSum);
-                                engineSum += Integer.parseInt(foundNumber);
-                                foundNumber = "";
-                                markerFound = false;
-                            } else {
-                                System.out.println("A number was found but it is not a schematic number");
-                                foundNumber = "";
-                                markerFound = false;
-                            }
+
+        for(int i = 0; i < charArray.length; i++){
+            int number = 0;
+            int digitCount = 0;
+            for(int j = charArray[i].length - 1; j >= 0; j--){
+                if(isNumber(charArray[i][j])){
+                    number += (int) (Integer.parseInt(String.valueOf(charArray[i][j])) * Math.pow(10, digitCount));
+                    digitCount++;
+                    if(j == 0){
+                        if (hasSchematicMarker2(j + 1, j + digitCount, i, charArray)) {
+                            System.out.println("Adding :" + number);
+                            engineSum += number;
                         }
-                    } catch (Exception e) {
-                        j++;
+                        number = 0;
+                        digitCount = 0;
                     }
                 }
-//                System.out.println("Currently in row : " + j + ". And at column : " + k + ".\n I have found a number : " + isNumber(charArray[j][k]) + ".\n I have found a schematic marker : " + isSchematicMarker(charArray[j][k]) + ".\n The current foundNumber is : " + foundNumber);
-                k++;
+                else{
+                    if(number != 0) {
+                        //System.out.println("Parameters:" + (j + 1) + " " + (j + digitCount) + " " + i);
+                        if (hasSchematicMarker2(j + 1, j + digitCount, i, charArray)) {
+                            System.out.println("Adding :" + number);
+                            engineSum += number;
+                        }
+                        number = 0;
+                        digitCount = 0;
+                    }
+                }
             }
-            j++;
         }
+
+        /*int j = rows - 1;
+
+        while (j > -1) {
+            int k = columns - 1;
+            int foundNumber = 0;
+            boolean markerFound = true;
+            int digitCounter = 0;
+            int firstindex = 0; //last digit in nr but first one found
+            int lastindex = 0; //first digit in nr but last one found
+            while (k > -1) {
+                if (isNumber(charArray[j][k])) {
+                    foundNumber += (int) (Integer.parseInt(String.valueOf(charArray[j][k])) * Math.pow(10, digitCounter));
+                    digitCounter++;
+
+                    if (digitCounter == 0) {
+                        firstindex = k;
+                    }
+                    if (k == 0) {
+                        System.out.println(foundNumber);
+                        engineSum += foundNumber;
+                        foundNumber = 0;
+                        digitCounter = 0;
+                        lastindex = k;
+                    } else if (!isNumber(charArray[j][k - 1])) {
+                        System.out.println(foundNumber);
+                        engineSum += foundNumber;
+                        foundNumber = 0;
+                        digitCounter = 0;
+                        lastindex = k;
+                    }
+                    markerFound = hasSchematicMarker2(lastindex, firstindex, j, charArray);
+                    if (markerFound) {
+                        System.out.println("hi");
+                    }
+                }
+                k--;
+            }
+            j--;
+        }*/
         System.out.println(engineSum);
+        System.out.println(Arrays.toString(findSymbols(charArray).toArray()));
     }
 
     static boolean isNumber(char checkme) {
@@ -152,4 +117,63 @@ public class Day3 {
         };
     }
 
+    static boolean hasSchematicMarker(int first, int last, int row, char[][] charArray) {
+//        int lengthofnr = (last +1) - first;
+        for (int c = -1; c < 2; c++) {
+            if (row == 0) {
+                c = 0;
+            }
+            for (int v = first; v < last + 1; v++) {
+                if (isSchematicMarker(charArray[row + c][v - 1])) {
+                    return true;
+                }
+            }
+            if (row == charArray.length - 1) {
+                break;
+            }
+        }
+        return false;
+    }
+
+    static boolean hasSchematicMarker2(int first, int last, int row, char[][] charArray) {
+        int pos1;
+        int pos2;
+        if(first == 0){
+            pos1 = first;
+        }
+        else {
+            pos1 = first - 1;
+        }
+
+        if(last == charArray[row].length - 1){
+            pos2 = last;
+        }
+        else {
+            pos2 = last + 1;
+        }
+        int currentRow = row - 1;
+        while(currentRow < charArray.length && currentRow <= row + 1) {
+            if(currentRow >= 0) {
+                for (int i = pos1; i <= pos2; i++) {
+                    if(isSchematicMarker(charArray[currentRow][i])){
+                        return true;
+                    }
+                }
+            }
+            currentRow++;
+        }
+        return false;
+    }
+
+    static Set<Character> findSymbols(char[][] arr){
+        Set<Character> list = new HashSet<>();
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr[i].length; j++){
+                if(!isNumber(arr[i][j]) && arr[i][j] != '.'){
+                    list.add(arr[i][j]);
+                }
+            }
+        }
+        return list;
+    }
 }
